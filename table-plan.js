60dba0ae938d239d2faaf64e6bbef44bce0c8cb9
Moritz Trah <moritz.trah@t-online.de>
2022-05-04 23:12:00 +0200

    initial commit

---
+++ table-plan.js
@@ -0,0 +1,937 @@
+const mapping = [
+  {
+    lastName: "Trah",
+    firstName: "Ferdinand",
+    name: "Ferdinand Trah",
+    seat: "T1-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Henri",
+    name: "Henri Trah",
+    seat: "T1-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Josephine",
+    name: "Josephine Trah",
+    seat: "T1-SPR1-S3",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Paul-Philipp",
+    name: "Paul-Philipp Trah",
+    seat: "T1-SPR1-S4",
+    food: "Fisch",
+  },
+  {
+    lastName: "Stutzke",
+    firstName: "Emil",
+    name: "Emil Stutzke",
+    seat: "T1-SPR2-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Katterbach",
+    firstName: "Maximilian",
+    name: "Maximilian Katterbach",
+    seat: "T1-SPR2-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Kandsberger",
+    firstName: "Louisa",
+    name: "Louisa Kandsberger",
+    seat: "T1-SPR2-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Well",
+    firstName: "Elisabeth",
+    name: "Elisabeth Well",
+    seat: "T2-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Reissberger",
+    firstName: "Moritz",
+    name: "Moritz Reissberger",
+    seat: "T2-SPR1-S2",
+    food: "Fisch",
+  },
+  {
+    lastName: "Meder",
+    firstName: "Siri",
+    name: "Siri Meder",
+    seat: "T2-SPR1-S3",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Geisler",
+    firstName: "Julian",
+    name: "Julian Geisler",
+    seat: "T2-SPR1-S4",
+    food: "Fisch",
+  },
+  {
+    lastName: "Well",
+    firstName: "Maximilian",
+    name: "Maximilian Well",
+    seat: "T2-SPR2-S1",
+    food: "Fisch",
+  },
+  {
+    lastName: "Knebel",
+    firstName: "Patricia",
+    name: "Patricia Knebel",
+    seat: "T2-SPR2-S2",
+    food: "Fisch",
+  },
+  {
+    lastName: "Steinmeyer",
+    firstName: "Julian",
+    name: "Julian Steinmeyer",
+    seat: "T2-SPR2-S3",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Kartini",
+    firstName: "Alissa",
+    name: "Alissa Kartini",
+    seat: "T2-SPR2-S4",
+    food: "Fisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Constantin",
+    name: "Constantin Trah",
+    seat: "T3-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Tietz",
+    firstName: "Victoria",
+    name: "Victoria Tietz",
+    seat: "T3-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Contzen",
+    firstName: "Tobias",
+    name: "Tobias Contzen",
+    seat: "T3-SPR1-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Müller",
+    firstName: "Lara",
+    name: "Lara Müller",
+    seat: "T3-SPR1-S4",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Schnizer",
+    firstName: "Lisa",
+    name: "Lisa Schnizer",
+    seat: "T3-SPR2-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Maas",
+    firstName: "Justine",
+    name: "Justine Maas",
+    seat: "T3-SPR2-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Contzen",
+    firstName: "Sina",
+    name: "Sina Contzen",
+    seat: "T3-SPR2-S3",
+    food: "Fisch",
+  },
+  {
+    lastName: "Müller",
+    firstName: "Christoph",
+    name: "Christoph Müller",
+    seat: "T3-SPR2-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Karlotta",
+    name: "Karlotta Trah",
+    seat: "T4-SPR1-S1",
+    food: "Fisch",
+  },
+  {
+    lastName: "Schneider",
+    firstName: "Moritz",
+    name: "Moritz Schneider",
+    seat: "T4-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Giefer",
+    firstName: "Annabell",
+    name: "Annabell Giefer",
+    seat: "T4-SPR1-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Manthei",
+    firstName: "Alexander",
+    name: "Alexander Manthei",
+    seat: "T4-SPR1-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Witt",
+    firstName: "Philipp",
+    name: "Philipp Witt",
+    seat: "T4-SPR2-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Heller",
+    firstName: "Antonia",
+    name: "Antonia Heller",
+    seat: "T4-SPR2-S2",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Giefer",
+    firstName: "Fabian",
+    name: "Fabian Giefer",
+    seat: "T4-SPR2-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Droste",
+    firstName: "Charlotte",
+    name: "Charlotte Droste",
+    seat: "T4-SPR2-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Karl-Hermann",
+    name: "Karl-Hermann Trah",
+    seat: "T5-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Katharina",
+    name: "Katharina Trah",
+    seat: "T5-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Paul",
+    name: "Paul Trah",
+    seat: "T5-SPR1-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Annette",
+    name: "Annette Trah",
+    seat: "T5-SPR1-S4",
+    food: "Fisch",
+  },
+  {
+    lastName: "Bagel-Trah",
+    firstName: "Simone",
+    name: "Simone Bagel-Trah",
+    seat: "T5-SPR1-S5",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Eduard",
+    name: "Eduard Trah",
+    seat: "T5-SPR1-S6",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Woidtke",
+    firstName: "Alexander",
+    name: "Alexander Woidtke",
+    seat: "T5-SPR2-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "König-Trah",
+    firstName: "Karola",
+    name: "Karola König-Trah",
+    seat: "T5-SPR2-S2",
+    food: "Fisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Dagmar",
+    name: "Dagmar Trah",
+    seat: "T5-SPR2-S3",
+    food: "Fisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Christoph",
+    name: "Christoph Trah",
+    seat: "T5-SPR2-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Karl",
+    name: "Karl Trah",
+    seat: "T5-SPR2-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Alice",
+    name: "Alice Trah",
+    seat: "T5-SPR2-S6",
+    food: "Fisch",
+  },
+  {
+    lastName: "Erdmann",
+    firstName: "Ina",
+    name: "Ina Erdmann",
+    seat: "T6-SPR1-S1",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Kleinfeld",
+    firstName: "Niklas",
+    name: "Niklas Kleinfeld",
+    seat: "T6-SPR1-S2",
+    food: "Fisch",
+  },
+  {
+    lastName: "Strelow",
+    firstName: "Michael",
+    name: "Michael Strelow",
+    seat: "T6-SPR1-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Strelow",
+    firstName: "Juliane",
+    name: "Juliane Strelow",
+    seat: "T6-SPR1-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Moritz",
+    name: "Moritz Trah",
+    seat: "T6-SPR1-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Strelow (sen.)",
+    firstName: "Michael",
+    name: "Michael Strelow (sen.)",
+    seat: "T6-SPR2-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Elisabeth",
+    name: "Elisabeth Trah",
+    seat: "T6-SPR2-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Strelow",
+    firstName: "Kurt",
+    name: "Kurt Strelow",
+    seat: "T6-SPR2-S3",
+    food: "Fisch",
+  },
+  {
+    lastName: "Strelow",
+    firstName: "Regina",
+    name: "Regina Strelow",
+    seat: "T6-SPR2-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Trah",
+    firstName: "Maximilian",
+    name: "Maximilian Trah",
+    seat: "T6-SPR2-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Korsten",
+    firstName: "Waltraud",
+    name: "Waltraud Korsten",
+    seat: "T7-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Korsten",
+    firstName: "Stefan",
+    name: "Stefan Korsten",
+    seat: "T7-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Geisler",
+    firstName: "Peter",
+    name: "Peter Geisler",
+    seat: "T7-SPR1-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Sonnenwald",
+    firstName: "Wolfgang",
+    name: "Wolfgang Sonnenwald",
+    seat: "T7-SPR1-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Schwarz",
+    firstName: "Georg",
+    name: "Georg Schwarz",
+    seat: "T7-SPR1-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Tewes",
+    firstName: "Hans-Bernd",
+    name: "Hans-Bernd Tewes",
+    seat: "T7-SPR1-S6",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Korsten",
+    firstName: "Günter",
+    name: "Günter Korsten",
+    seat: "T7-SPR2-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Korsten",
+    firstName: "Stephanie",
+    name: "Stephanie Korsten",
+    seat: "T7-SPR2-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Geisler",
+    firstName: "Uta",
+    name: "Uta Geisler",
+    seat: "T7-SPR2-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Sonnenwald",
+    firstName: "Helga",
+    name: "Helga Sonnenwald",
+    seat: "T7-SPR2-S4",
+    food: "Fisch",
+  },
+  {
+    lastName: "Irowski",
+    firstName: "Manfred",
+    name: "Manfred Irowski",
+    seat: "T7-SPR2-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Pastoors",
+    firstName: "Klaudia",
+    name: "Klaudia Pastoors",
+    seat: "T7-SPR2-S6",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Karl",
+    firstName: "Charlotte",
+    name: "Charlotte Karl",
+    seat: "T8-SPR1-S1",
+    food: "Spaghetti",
+  },
+  {
+    lastName: "Sonnenwald",
+    firstName: "Verena",
+    name: "Verena Sonnenwald",
+    seat: "T8-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Klätte-Rudolf",
+    firstName: "Angela",
+    name: "Angela Klätte-Rudolf",
+    seat: "T8-SPR1-S3",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Hahn",
+    firstName: "Thomas",
+    name: "Thomas Hahn",
+    seat: "T8-SPR1-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Karl",
+    firstName: "Andreas",
+    name: "Andreas Karl",
+    seat: "T8-SPR2-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Rustler-Trah",
+    firstName: "Leonora",
+    name: "Leonora Rustler-Trah",
+    seat: "T8-SPR2-S2",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Rustler",
+    firstName: "Markus",
+    name: "Markus Rustler",
+    seat: "T8-SPR2-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Schwarz",
+    firstName: "Anja",
+    name: "Anja Schwarz",
+    seat: "T8-SPR2-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Ricken",
+    firstName: "Sebastian",
+    name: "Sebastian Ricken",
+    seat: "T9-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Sigel",
+    firstName: "Katharina",
+    name: "Katharina Sigel",
+    seat: "T9-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Bär",
+    firstName: "Florian",
+    name: "Florian Bär",
+    seat: "T9-SPR1-S3",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Guse",
+    firstName: "Fabian",
+    name: "Fabian Guse",
+    seat: "T9-SPR1-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Tiele",
+    firstName: "Cecile",
+    name: "Cecile Tiele",
+    seat: "T9-SPR2-S1",
+    food: "Fisch",
+  },
+  {
+    lastName: "Richle-Franzen",
+    firstName: "Thomas",
+    name: "Thomas Richle-Franzen",
+    seat: "T9-SPR2-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Raber",
+    firstName: "Eva",
+    name: "Eva Raber",
+    seat: "T9-SPR2-S3",
+    food: "Fisch",
+  },
+  {
+    lastName: "Pallas",
+    firstName: "Florian",
+    name: "Florian Pallas",
+    seat: "T9-SPR2-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Kaas",
+    firstName: "Stefan",
+    name: "Stefan Kaas",
+    seat: "T10-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Meyer-Spick",
+    firstName: "Stefan",
+    name: "Stefan Meyer-Spick",
+    seat: "T10-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Ramrath",
+    firstName: "Carolin",
+    name: "Carolin Ramrath",
+    seat: "T10-SPR1-S3",
+    food: "Fisch",
+  },
+  {
+    lastName: "Von der Mark",
+    firstName: "Mira",
+    name: "Mira Von der Mark",
+    seat: "T10-SPR1-S4",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Düppenbecker",
+    firstName: "Daniel",
+    name: "Daniel Düppenbecker",
+    seat: "T10-SPR1-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Hausner",
+    firstName: "Victoria",
+    name: "Victoria Hausner",
+    seat: "T10-SPR1-S6",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Iskander",
+    firstName: "Viola",
+    name: "Viola Iskander",
+    seat: "T10-SPR2-S1",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Bürkle",
+    firstName: "Gregor",
+    name: "Gregor Bürkle",
+    seat: "T10-SPR2-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Erpenbach",
+    firstName: "Alina",
+    name: "Alina Erpenbach",
+    seat: "T10-SPR2-S3",
+    food: "Fisch",
+  },
+  {
+    lastName: "Shalaby",
+    firstName: "Mohammad",
+    name: "Mohammad Shalaby",
+    seat: "T10-SPR2-S4",
+    food: "Fisch",
+  },
+  {
+    lastName: "Jansen",
+    firstName: "Lena",
+    name: "Lena Jansen",
+    seat: "T10-SPR2-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Matena",
+    firstName: "Andreas",
+    name: "Andreas Matena",
+    seat: "T10-SPR2-S6",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Kühne",
+    firstName: "Cinthia",
+    name: "Cinthia Kühne",
+    seat: "T11-SPR1-S1",
+    food: "Fisch",
+  },
+  {
+    lastName: "Kühne",
+    firstName: "Fabian",
+    name: "Fabian Kühne",
+    seat: "T11-SPR1-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Weerd",
+    firstName: "Jana",
+    name: "Jana Weerd",
+    seat: "T11-SPR1-S3",
+    food: "Fisch",
+  },
+  {
+    lastName: "Sigg",
+    firstName: "Juliana",
+    name: "Juliana Sigg",
+    seat: "T11-SPR1-S4",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Steuerwald",
+    firstName: "Angelina",
+    name: "Angelina Steuerwald",
+    seat: "T11-SPR1-S5",
+    food: "Fisch",
+  },
+  {
+    lastName: "Lowey",
+    firstName: "Roland",
+    name: "Roland Lowey",
+    seat: "T11-SPR1-S6",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Espino",
+    firstName: "Tirana",
+    name: "Tirana Espino",
+    seat: "T11-SPR1-S7",
+    food: "Fisch",
+  },
+  {
+    lastName: "Corthouts",
+    firstName: "Sophie",
+    name: "Sophie Corthouts",
+    seat: "T11-SPR2-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Slama",
+    firstName: "Jana",
+    name: "Jana Slama",
+    seat: "T11-SPR2-S2",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Mollenkott",
+    firstName: "Philipp",
+    name: "Philipp Mollenkott",
+    seat: "T11-SPR2-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Frick",
+    firstName: "Lena",
+    name: "Lena Frick",
+    seat: "T11-SPR2-S4",
+    food: "Fisch",
+  },
+  {
+    lastName: "Lowey",
+    firstName: "Emilie",
+    name: "Emilie Lowey",
+    seat: "T11-SPR2-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Reuter",
+    firstName: "Marcus",
+    name: "Marcus Reuter",
+    seat: "T11-SPR2-S6",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Groß",
+    firstName: "Felix",
+    name: "Felix Groß",
+    seat: "T12-SPR1-S1",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Stangel",
+    firstName: "Ina",
+    name: "Ina Stangel",
+    seat: "T12-SPR1-S2",
+    food: "Fisch",
+  },
+  {
+    lastName: "Hanschke",
+    firstName: "Matthias",
+    name: "Matthias Hanschke",
+    seat: "T12-SPR1-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Hollstein",
+    firstName: "Maximilian",
+    name: "Maximilian Hollstein",
+    seat: "T12-SPR1-S4",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Sterkel",
+    firstName: "Christoph",
+    name: "Christoph Sterkel",
+    seat: "T12-SPR1-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Limper",
+    firstName: "Anne",
+    name: "Anne Limper",
+    seat: "T12-SPR1-S6",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Vallee",
+    firstName: "Caroline",
+    name: "Caroline Vallee",
+    seat: "T12-SPR2-S1",
+    food: "Fisch",
+  },
+  {
+    lastName: "Groborz",
+    firstName: "Arthur",
+    name: "Arthur Groborz",
+    seat: "T12-SPR2-S2",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Wächter",
+    firstName: "Carolin",
+    name: "Carolin Wächter",
+    seat: "T12-SPR2-S3",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Schmid",
+    firstName: "Viktoria",
+    name: "Viktoria Schmid",
+    seat: "T12-SPR2-S4",
+    food: "Vegetarisch",
+  },
+  {
+    lastName: "Rautzenberg",
+    firstName: "Lorena",
+    name: "Lorena Rautzenberg",
+    seat: "T12-SPR2-S5",
+    food: "Fleisch",
+  },
+  {
+    lastName: "Rautzenberg",
+    firstName: "Christopher",
+    name: "Christopher Rautzenberg",
+    seat: "T12-SPR2-S6",
+    food: "Fleisch",
+  },
+];
+
+let landingContentElement;
+let searchBoxElement;
+let searchResultContainerElement;
+
+let guest;
+
+window.onload = (e) => {
+  landingContentElement = document.getElementById("landing-content");
+  searchBoxElement = document.getElementById("search-box");
+  searchResultContainerElement = document.getElementById("search-results");
+
+  searchBoxElement.oninput = (e) => {
+    clearMatchesFromSvg();
+    clearMatchesFromResultList();
+    let searchValue = searchBoxElement.value.toLowerCase();
+    if (searchValue.length == 0) return;
+
+    var searchResult = mapping.filter((entry) => {
+      var lowerCaseName = entry.name.toLowerCase();
+      return lowerCaseName.indexOf(searchValue) != -1;
+    });
+
+    if (searchResult.length > 20) return;
+
+    if (searchResult.length > 0) {
+      searchResult.forEach((element) => {
+        addSearchResult(element);
+      });
+    }
+  };
+};
+
+let clearMatchesFromSvg = () => {
+  mapping.forEach((element) => {
+    var seat = document.getElementById(element.seat);
+    if (seat) {
+      seat.classList.remove("search-match");
+    }
+  });
+};
+
+let clearMatchesFromResultList = () => {
+  searchResultContainerElement.replaceChildren();
+};
+
+let addSearchResult = (match) => {
+  var resultElement = document.createElement("div");
+  resultElement.classList.add("search-result");
+  resultElement.onclick = () => selectGuest(match);
+
+  var nameElement = document.createElement("span");
+  nameElement.innerText = match.name;
+  resultElement.appendChild(nameElement);
+  searchResultContainerElement.appendChild(resultElement);
+};
+
+let selectGuest = (match) => {
+  guest = match;
+  addNameToMarkUp(match);
+  markSeat(match);
+  addMenuItem(match);
+  makeSectionsVisible();
+  landingContentElement.classList.add("guest-selected");
+};
+
+let makeSectionsVisible = () => {
+  var sectionElements = document.getElementsByClassName("page-section");
+  for (let i = 0; i < sectionElements.length; i++) {
+    const element = sectionElements.item(i);
+    element.classList.add("visible");
+  }
+  var navbarElement = document.getElementById("navigation-bar");
+  navbarElement.classList.add("visible");
+};
+
+let addNameToMarkUp = (match) => {
+  var guestNameElements = document.getElementsByClassName("guest-name");
+  for (var i = 0; i < guestNameElements.length; i++) {
+    var e = guestNameElements.item(i);
+    e.innerHTML = match.firstName;
+  }
+};
+
+let markSeat = (match) => {
+  var seat = document.getElementById(match.seat);
+  seat.classList.add("search-match");
+};
+
+let addMenuItem = (match) => {
+  var menuItem = document.getElementById("variable-menu-item");
+
+  let text;
+  switch (match.food) {
+    case "Fleisch":
+      text = "Rinderfilet | Kürbis | Karotte | Kartoffel";
+      break;
+    case "Fisch":
+      text = "Gebratener Kabeljau | Pastinake | Kürbis | Öle";
+      break;
+    case "Vegetarisch":
+      text = "Risotto | Bete | Spitzkohl | Zitrus";
+      break;
+  }
+
+  var container = document.createElement("div");
+  container.innerHTML = text;
+  menuItem.appendChild(container);
+};
